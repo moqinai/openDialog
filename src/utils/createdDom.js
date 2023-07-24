7e7e5856f792cda7d31591b5cbda0df8c8af6444
lipengcheng <lipengcheng@zonst.cn>
2023-07-25 00:58:18 +0800

    Dom css

--- src/utils/createdDom.js
+++ src/utils/createdDom.js
@@ -1,9 +1,8 @@
 const dom = `
-  <div class="zonst-switch-system" style="z-index: 2099;">
-    <div role="dialog" aria-modal="true" aria-label="Tips" class="el-overlay-dialog">
-      <div class="el-dialog" tabindex="-1" style="--el-dialog-width: 30%;">
+    <div role="dialog" aria-modal="true" aria-label="系统切换" class="el-overlay-dialog">
+      <div class="el-dialog" tabindex="-1">
         <header class="el-dialog__header">
-          <span role="heading" class="el-dialog__title">Tips</span>
+          <span role="heading" class="el-dialog__title">系统切换</span>
           <button aria-label="Close this dialog" class="el-dialog__headerbtn" type="button">
             <i class="el-icon el-dialog__close">
               <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
@@ -14,23 +13,25 @@ const dom = `
           </button>
         </header>
         <div id="el-id-9829-89" class="el-dialog__body">
-          <span data-v-1735ddb7="">This is a message</span>
+          <span>This is a message</span>
         </div>
         <footer class="el-dialog__footer">
-          <span class="dialog-footer" data-v-1735ddb7="">
-            <button aria-disabled="false" type="button" class="el-button" data-v-1735ddb7="">
+          <span class="dialog-footer">
+            <button aria-disabled="false" type="button" class="el-button">
               <span class="">取消</span>
             </button>
-            <button aria-disabled="false" type="button" class="el-button el-button--primary" data-v-1735ddb7="">
+            <button aria-disabled="false" type="button" class="el-button el-button--primary">
               <span class=""> 确定 </span>
             </button>
           </span>
         </footer>
       </div>
     </div>
-  </div>
 `
-const domdialog = document.createElement('div').innerHTML = dom
+const domdialog = document.createElement('div')
+domdialog.className = 'zonst-switch-system'
+domdialog.style.zIndex = '2099'
+domdialog.innerHTML = dom
 document.body.appendChild(domdialog)
 
 
